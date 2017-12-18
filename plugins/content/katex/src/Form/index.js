9c40d212698925c39f14d444a8ac2ddeb816d6ee
Knorrke <knorr.b@gmx.de>
2017-12-18 15:01:18 +0100

    add checkbox to switch between inline and block

--- plugins/content/katex/src/Form/index.js
+++ plugins/content/katex/src/Form/index.js
@@ -1,31 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
 import debounce from 'lodash.debounce'
 import Portal from 'react-portal'
 import position from 'selection-position'
-import {darkBlack} from 'material-ui/styles/colors'
+import { darkBlack } from 'material-ui/styles/colors'
 
 class Form extends Component {
   constructor(props) {
     super(props)
-    const {formula} = this.props
+    const { formula, inline } = this.props
     this.state = {
-      formula: formula
+      formula: formula,
+      inline: inline
     }
     this.renderMath = debounce(this.renderMath, 500)
   }
 
   componentWillReceiveProps(nextProps) {
-    const {formula} = nextProps
+    const { formula, inline } = nextProps
     this.setState({
-      formula: formula
+      formula: formula,
+      inline: inline
     })
   }
 
-  handleChange = (e, newFormula) => {
+  handleFormulaChange = (e, newFormula) => {
     this.setState({
       formula: newFormula
     })
@@ -33,42 +36,72 @@ class Form extends Component {
     this.renderMath(newFormula)
   }
 
-  renderMath = (newFormula) => {
-    console.log("render called");
-    const {editor, node} = this.props
-    const {key} = node
-
-    const next = this.props.editor.getState().transform().setNodeByKey(key, {
-      data: {
-        formula: newFormula
-      }
-    }).apply()
+  handleInlineChange = (e, inline) => {
+    this.setState({
+      inline: inline
+    })
+    const { editor, node } = this.props
+    const { key } = node
+    const next = editor
+      .getState()
+      .transform()
+      .setNodeByKey(key, {
+        data: {
+          formula: this.state.formula,
+          inline: inline
+        }
+      })
+      .apply()
 
     editor.onChange(next)
     this.focusTextfield()
   }
 
-  focusTextfield = () => {setTimeout(() => {this.input && this.input.focus()}, 0)}
+  renderMath = newFormula => {
+    const { editor, node } = this.props
+    const { key } = node
+
+    const next = editor
+      .getState()
+      .transform()
+      .setNodeByKey(key, {
+        data: {
+          formula: newFormula,
+          inline: this.state.inline
+        }
+      })
+      .apply()
+
+    editor.onChange(next)
+    this.focusTextfield()
+  }
+
+  focusTextfield = () => {
+    setTimeout(() => {
+      this.input && this.input.focus()
+    }, 0)
+  }
 
   handleOpen = portal => {
     const textfield = portal.firstChild
-    const { top } = position();
+    const { top } = position()
     textfield.style.opacity = 1
-    textfield.style.top = `${top + window.scrollY - textfield.offsetHeight }px`
-    textfield.style.left = `${document.body.clientWidth/2 - textfield.offsetWidth/2}px`
+    textfield.style.top = `${top + window.scrollY - textfield.offsetHeight}px`
+    textfield.style.left = `${document.body.clientWidth / 2 -
+      textfield.offsetWidth / 2}px`
   }
 
-  focusReference = (input) => {
-    if(input) this.input = input
+  focusReference = input => {
+    if (input) this.input = input
   }
 
   render() {
-    const {node, state} = this.props
+    const { node, state } = this.props
     const isActive = state.isFocused && state.selection.hasEdgeIn(node)
 
     return (
       <Portal isOpened={isActive} onOpen={this.handleOpen}>
-          <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
           <div
             className="ory-prevent-blur ory-plugins-content-slate-inline-toolbar"
             style={{
@@ -83,12 +116,21 @@ class Form extends Component {
               ref={this.focusReference}
               hintText={'\\frac{1}{2}'}
               floatingLabelText="Formula"
-              inputStyle={{color: 'white'}}
-              floatingLabelStyle={{color: 'white'}}
-              hintStyle={{color: 'grey'}}
-              style={{width: '800px'}}
+              inputStyle={{ color: 'white' }}
+              floatingLabelStyle={{ color: 'white' }}
+              hintStyle={{ color: 'grey' }}
+              style={{ width: '800px' }}
               value={this.state.formula}
-              onChange={this.handleChange}
+              onChange={this.handleFormulaChange}
+            />
+
+            <Checkbox
+              checked={this.state.inline}
+              label="Show as inline element"
+              labelStyle={{ color: 'white' }}
+              iconStyle={{ fill: 'white' }}
+              style={{ margin: '10px' }}
+              onCheck={this.handleInlineChange}
             />
           </div>
         </MuiThemeProvider>
@@ -96,6 +138,5 @@ class Form extends Component {
     )
   }
 }
-
 
 export default Form
--- plugins/content/katex/src/index.js
+++ plugins/content/katex/src/index.js
@@ -2,10 +2,9 @@
 import KatexIcon from 'material-ui/svg-icons/editor/functions'
 import React from 'react'
 
-import {ToolbarButton} from 'ory-editor-plugins-slate/lib/helpers'
+import { ToolbarButton } from 'ory-editor-plugins-slate/lib/helpers'
 import Plugin from 'ory-editor-plugins-slate/lib/plugins/Plugin'
-import KatexBlock from './block'
-import KatexInline from './inline'
+import Katex from './katex'
 
 import './index.css'
 
@@ -22,11 +21,15 @@ export default class KatexPlugin extends Plugin {
 
   // eslint-disable-next-line react/display-name
   Button = ({ editorState, onChange }) => {
-    const onClick = (e) => {
+    const onClick = e => {
       e.preventDefault()
 
-      const hasBlock = editorState.blocks.some((block) => block.type === KATEX_BLOCK)
-      const hasInline = editorState.inlines.some((inline: any) => inline.type === KATEX_INLINE)
+      const hasBlock = editorState.blocks.some(
+        block => block.type === KATEX_BLOCK
+      )
+      const hasInline = editorState.inlines.some(
+        (inline: any) => inline.type === KATEX_INLINE
+      )
 
       let newState
 
@@ -45,7 +48,7 @@ export default class KatexPlugin extends Plugin {
           .transform()
           .insertInline({
             type: KATEX_INLINE,
-            data: {formula: ' '}, //hack... empty string doesn't work. FIXME
+            data: { formula: ' ', inline: true }, //hack... empty string doesn't work. FIXME
             isVoid: true
           })
           .apply()
@@ -57,23 +60,30 @@ export default class KatexPlugin extends Plugin {
      const inlineBlocks = editorState.blocks.filter((inline) => inline.type === KATEX_INLINE)
      */
 
-    const hasMath = editorState.blocks.some((block) => block.type === KATEX_BLOCK)
-    const hasInline = editorState.inlines.some((inline) => inline.type === KATEX_INLINE)
+    const hasMath = editorState.blocks.some(block => block.type === KATEX_BLOCK)
+    const hasInline = editorState.inlines.some(
+      inline => inline.type === KATEX_INLINE
+    )
 
-    return <ToolbarButton onClick={onClick} isActive={hasMath || hasInline} icon={<KatexIcon />}/>
+    return (
+      <ToolbarButton
+        onClick={onClick}
+        isActive={hasMath || hasInline}
+        icon={<KatexIcon />}
+      />
+    )
   }
 
   name = 'katex'
 
   nodes = {
-    [KATEX_BLOCK]: KatexBlock,
-    [KATEX_INLINE]: KatexInline
+    [KATEX_BLOCK]: Katex,
+    [KATEX_INLINE]: Katex
   }
 
   toolbarButtons = [this.Button]
 
   deserialize(el, next) {
-    //console.log('deserializing', el);
     switch (el.tagName.toLowerCase()) {
       case 'katexblock':
         return {
@@ -81,7 +91,8 @@ export default class KatexPlugin extends Plugin {
           type: KATEX_BLOCK,
           isVoid: true,
           data: {
-            formula: el.childNodes[0].value
+            formula: el.childNodes[0].value,
+            inline: false
           }
         }
       case 'katexinline':
@@ -90,18 +101,14 @@ export default class KatexPlugin extends Plugin {
           type: KATEX_INLINE,
           isVoid: true,
           data: {
-            formula: el.childNodes[0].value
+            formula: el.childNodes[0].value,
+            inline: true
           }
         }
     }
   }
 
   serialize(object, children) {
-    console.log('serializing', object);
-    if (object.kind !== 'block') {
-      return
-    }
-
     switch (object.type) {
       case KATEX_BLOCK:
         return <katexblock>{children}</katexblock>
