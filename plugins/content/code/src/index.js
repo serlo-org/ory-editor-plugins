daf4d37cf5cf8e9951dfb04ae03a8fac42821988
FWirtz <florian.wirtz08@gmail.com>
2018-01-09 15:55:57 +0100

    :speech_balloon: Improve documentation and naming

--- plugins/content/code/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Highlight from "./Component";
-
-export default {
-    Component: Highlight,
-    name: "my/wobbly/ass",
-    version: "0.1.0",
-    text: "Code Highlight",
-}
